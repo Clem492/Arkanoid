478a5aa1553ca2c46d5490699ef304a745e71626
Clem492 <c.turpin@rubika-edu.re>
2026-02-18 16:21:14 +0400

    ajout du rebon mais pas encore fonctionnel

--- Assets/script/BalleScript.cs
+++ Assets/script/BalleScript.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class BalleScript : MonoBehaviour
 {
     [SerializeField] int ballSpeed;
 
-    public Vector3 direction;
-
     private bool firstShoot = true;
 
     RaycastHit hit;
+
+   public Vector2 currentDirection;
 
     // Update is called once per frame
     void Update()
@@ -23,28 +24,62 @@ public class BalleScript : MonoBehaviour
         {
             transform.Translate(Vector3.down * Time.deltaTime * ballSpeed);
         }
+        Move();
     }
 
     private void Move()
     {
-        transform.Translate(direction.x, direction.y, direction.z);
+        if (!firstShoot)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * ballSpeed);
+        }
         
     }
 
+   private void calculateTrajectoir()
+    {
+        Vector3 origin = transform.position;
+        Vector3 dir = transform.right;
+
+        Ray ray = new Ray(origin, dir);
+/*        Gizmos.DrawLine(origin, origin + dir);*/
+        //tirer un raycast faire le produit scalaire entre les obstacles
+        for (int i = 0; i < 200; i++)
+        {
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                /*Gizmos.DrawLine(origin, hit.point);
+                Gizmos.DrawSphere(hit.point, 0.1f);*/
+                Vector3 normale = hit.normal;
+
+                Vector3 reflet = Reflect(ray.direction, normale);
+                /*Gizmos.DrawLine(hit.point, (Vector2)hit.point + reflet * 4);*/
+                origin = hit.point;
+                dir = reflet;
+                ray = new Ray(origin, dir);
+                transform.rotation = Quaternion.Euler(dir);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    Vector2 Reflect(Vector2 direction, Vector2 normale)
+    {
+        return direction - 2 * Vector2.Dot(normale, direction) * normale;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             firstShoot = false;
-            Vector2 normal = (new Vector2(transform.position.x, transform.position.y) - collision.ClosestPoint(transform.position)).normalized;
-            direction = Vector2.Reflect(direction, normal);
+            
         }
-
+        calculateTrajectoir();
     }   
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawRay(transform.position, direction);
-    }
+
 
 }
